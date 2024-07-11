e3cbb15362b5c779d6c39a794f8b87fd0d07db97
owntheweb <67289492+owntheweb@users.noreply.github.com>
2024-07-10 19:09:09 -0600

    install and configure Material UI
    I chose to install MUI Icons and also all of MUI. I recall the
    team enjoying the use of MUI as I'm growing to like it a lot.
    There's also risk of it being overkill for this project if it
    was to exist as-is and not grow. However, I could see this
    scaling up quickly where MUI would be very helpful.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './App.css';
 
 const App = (): React.ReactNode => {
-  return <div>{/* POKEMONS */}</div>;
+  return <div>POKEMONS!!</div>;
 };
 
 export default App;
