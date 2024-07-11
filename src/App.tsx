08a24ee3b72b243aaa51e46ec07217bc3f53ed18
owntheweb <67289492+owntheweb@users.noreply.github.com>
2024-07-10 19:47:31 -0600

    install tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './App.css';
 
+const queryClient = new QueryClient();
+
 const App = (): React.ReactNode => {
-  return <div>POKEMONS!!</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>POKEMONS!!</div>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
